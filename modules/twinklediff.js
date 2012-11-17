b50146e129d169d417011495b300db304bde0faf
Jayanta Nath <jayantanth@gmail.com>
2012-11-17 18:30:01 +0530

    translation upadte

--- modules/twinklediff.js
+++ modules/twinklediff.js
@@ -6,37 +6,38 @@
  * Active on:              Existing non-special pages
  * Config directives in:   TwinkleConfig
  */
-
+ 
 Twinkle.diff = function twinklediff() { 
 	if( mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId') ) {
 		return;
 	}
-
+ 
 	var query = {
 		'title': mw.config.get('wgPageName'),
 		'diff': 'cur',
 		'oldid': 'prev'
 	};
-
-	twAddPortletLink( mw.util.wikiScript("index")+ "?" + $.param( query ), 'Last', 'tw-lastdiff', 'Show most recent diff' );
-
+ 
+	twAddPortletLink( mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/index.php?' + QueryString.create( query ), 'শেষ পরিবর্তন', 'tw-lastdiff', 'সর্বশেষ পরিবর্তন/পার্থক্য দেখাও' );
+ 
 	// Show additional tabs only on diff pages
-	if (Morebits.queryString.exists('diff')) {
-		twAddPortletLink(function(){ Twinkle.diff.evaluate(false); }, 'Since', 'tw-since', 'Show difference between last diff and the revision made by previous user' );
-		twAddPortletLink( function(){ Twinkle.diff.evaluate(true); }, 'Since mine', 'tw-sincemine', 'Show difference between last diff and my last revision' );
-
-		var oldid = /oldid=(.+)/.exec($('#mw-diff-ntitle1').find('strong a').first().attr("href"))[1];
+	if (QueryString.exists('diff')) {
+		$(twAddPortletLink("#", 'অদ্যাবধি', 'tw-since', 'আগের ব্যবহারকারির পরিবর্তন থেকে অদ্যাবধি পরিবর্তন/পার্থক্য  দেখাও' )).click(function(){Twinkle.diff.evaluate(false);});
+		$(twAddPortletLink("#", 'আমার অদ্যাবধি ', 'tw-sincemine', 'সর্বশেষ পরিবর্তন ও আমার কাজের মধ্যে পরিবর্তন/পার্থক্য  দেখাও' )).click(function(){Twinkle.diff.evaluate(true);});
+ 
+		var oldid = /oldid=(.+)/.exec($('div#mw-diff-ntitle1 strong a').first().attr("href"))[1];
 		query = {
 			'title': mw.config.get('wgPageName'),
 			'diff': 'cur',
 			'oldid' : oldid
 		};
-		twAddPortletLink( mw.util.wikiScript("index")+ "?" + $.param( query ), 'Current', 'tw-curdiff', 'Show difference to current revision' );
+		twAddPortletLink( mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/index.php?' + QueryString.create( query ), 'বর্তমান', 'tw-curdiff', 'বর্তমান সংস্করণের পরিবর্তন/পার্থক্য দেখাও' );
 	}
 };
-
+ 
 Twinkle.diff.evaluate = function twinklediffEvaluate(me) {
-
+	var ntitle = getElementsByClassName( document.getElementById('bodyContent'), 'td' , 'diff-ntitle' )[0];
+ 
 	var user;
 	if( me ) {
 		user = mw.config.get('wgUserName');
@@ -57,17 +58,17 @@ Twinkle.diff.evaluate = function twinklediffEvaluate(me) {
 		'rvstartid': mw.config.get('wgCurRevisionId') - 1, // i.e. not the current one
 		'rvuser': user
 	};
-	Morebits.status.init( document.getElementById('bodyContent') );
-	var wikipedia_api = new Morebits.wiki.api( 'Grabbing data of initial contributor', query, Twinkle.diff.callbacks.main );
+	Status.init( document.getElementById('bodyContent') );
+	var wikipedia_api = new Wikipedia.api( 'প্রথম অবদানকারির উপাত্ত সংকলিত করা হচ্ছে', query, Twinkle.diff.callbacks.main );
 	wikipedia_api.params = { user: user };
 	wikipedia_api.post();
 };
-
+ 
 Twinkle.diff.callbacks = {
 	main: function( self ) {
 		var xmlDoc = self.responseXML;
 		var revid = $(xmlDoc).find('rev').attr('revid');
-
+ 
 		if( ! revid ) {
 			self.statelem.error( 'no suitable earlier revision found, or ' + self.params.user + ' is the only contributor. Aborting.' );
 			return;
@@ -77,6 +78,6 @@ Twinkle.diff.callbacks = {
 			'oldid': revid,
 			'diff': mw.config.get('wgCurRevisionId')
 		};
-		window.location = mw.util.wikiScript('index') + '?' + Morebits.queryString.create( query );
+		window.location = mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/index.php?' + QueryString.create( query );
 	}
 };
